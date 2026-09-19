7cee4ae5c68f060c444e4ee459965d0208658b2b
YaseenWorkspace <yaseena.ali@outlook.com>
2026-09-19 23:49:04 +0100

    Added an  AMI ID variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,4 +10,8 @@ variable "instance_type" {
     default     = "t3.micro"
 }
 
-
+variable "ami_id" {
+    description = "The AMI ID to use for the EC2 instance"
+    type        = string
+    default     = "ami-12345678"
+}
