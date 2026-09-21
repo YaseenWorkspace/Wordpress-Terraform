5542860af37ca8bb76256c61ce4a3a3c1446915b
YaseenWorkspace <yaseena.ali@outlook.com>
2026-09-21 14:54:47 +0100

    Added dnf udpate as per amazon linux 2023 mariadb guide

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -5,9 +5,9 @@ db_name="wordpress"
 db_username="wordpressuser"
 # In a real-world scenario, you should use a more secure method to handle passwords, such as using AWS Secrets Manager or environment variables. For this example, we will hardcode the password for simplicity.
 db_user_password="MyStrongPassword123!"
-
-# Update the system and install necessary packages
-yum update -y
+db_host="localhost"
+# Update the system 
+dnf update -y
 #yum is the package manager and can install packages , update and delete packages.
 yum install -y httpd
 #systemctl manages services on the system and start this service immediately and enable it to start on boot.
@@ -38,12 +38,12 @@ mysql -u root -e "CREATE USER '$db_username'@'localhost' IDENTIFIED BY '$db_user
 mysql -u root -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_username'@'localhost';"
 
 
-
+#creates WordPress's configuration file and fills it with the database details WordPress needs to connect to MariaDB
 cp wordpress/wp-config-sample.php wordpress/wp-config.php
 sed -i "s/database_name_here/$db_name/g" wordpress/wp-config.php
 sed -i "s/username_here/$db_username/g" wordpress/wp-config.php
 sed -i "s/password_here/$db_user_password/g" wordpress/wp-config.php
-sed -i "s/localhost/${DB_HOST}/g" wordpress/wp-config.php
+sed -i "s/localhost/${db_host}/g" wordpress/wp-config.php
 
 #chown -R httpd:httpd /usr/share/httpd/html
 #chmod -R 755 /usr/share/httpd/html
